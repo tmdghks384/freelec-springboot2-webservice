eb870cf47357e56fe8f4f830feae71ae0b128804
tmdghks384 <74952589+tmdghks384@users.noreply.github.com>
2021-02-23 13:29:03 +0900

    등록 오류해결

--- src/main/java/com/jojoldu/book/springboot/web/dto/HelloResponseDto.java
+++ src/main/java/com/jojoldu/book/springboot/web/dto/HelloResponseDto.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public class HelloResponseDto {
+
     private final String name;
     private final int amount;
+
 }
--- src/test/java/com/jojoldu/book/springboot/web/dto/HelloResponseDtoTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/dto/HelloResponseDtoTest.java
@@ -1,11 +1,13 @@
 package com.jojoldu.book.springboot.web.dto;
 
 import org.junit.Test;
-import static org.assertj.core.api.Assertions.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class HelloResponseDtoTest {
+
     @Test
-    public void 롬복_기능_테스트(){
+    public void 롬복_기능_테스트() {
         //given
         String name = "test";
         int amount = 1000;
